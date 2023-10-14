b55b2a4f68c40f77d8bb75ada51e7ed82a324385
Yulia Gladysheva <yulliagladysheva@gmail.com>
2023-10-15 03:01:26 +0600

    add smooth scroll and scrollbar

--- src/components/header/header.ts
+++ src/components/header/header.ts
@@ -2,3 +2,12 @@ document.querySelector(".hamburger").addEventListener("click", function () {
     document.querySelector(".header").classList.toggle("mobile");
     document.querySelector("body").classList.toggle("hidden");
 });
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
