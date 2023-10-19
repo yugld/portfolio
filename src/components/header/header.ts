4ad4d9ecbfda1625023f6eadacbdad37c4404949
Yulia Gladysheva <yulliagladysheva@gmail.com>
2023-10-19 14:46:10 +0600

    edit header

--- src/components/header/header.ts
+++ src/components/header/header.ts
@@ -2,9 +2,6 @@ document.querySelector(".hamburger").addEventListener("click", function () {
     document.querySelector(".header").classList.toggle("mobile");
     document.querySelector("body").classList.toggle("hidden");
 });
-document.querySelector(".menu_item").addEventListener("click", function () {
-    document.querySelector(".header").classList.toggle("mobile");
-});
 
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
